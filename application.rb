cfad51cba4f4a4167f2fe583adca40acb69d38eb
ippa <iplord@iplord.com>
2010-01-29 23:20:41 +0100

    support haml (index.haml), textile (.textile) and markdown (.md)

--- application.rb
+++ application.rb
@@ -9,6 +9,17 @@ end
 
 get '/:presentation' do 
   @title = "#{params[:presentation]}!  [made with http://github.com/ippa/flaunt]"
-  erb :"#{params[:presentation]}/index"
+
+  index = Dir["presentations/#{params[:presentation]}/index.*"].first
+  content = open(index).read
+
+  html = case File.extname(index)
+    when ".erb"       then require 'erb';       ERB.new(content, 0, "%<>").result
+    when ".haml"      then require 'haml';      Haml::Engine.new(content).render
+    when ".textile"   then require 'RedCloth';  RedCloth.new(content).to_html
+    when ".md"        then require 'maruku';    Maruku.new(content).to_html
+  end
+  
+  erb html  # render the produced HTML with our layout.erb
 end
 
---
+++ presentations/markdown-example/index.md
@@ -0,0 +1,7 @@
+<div markdown="1" class="slide">
+Flaunt does Markdown
+====================
+* test
+* test
+* test
+</div>
