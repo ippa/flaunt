d17673fe0382bab8a9c3b61a89c90a776e4ec802
ippa <iplord@iplord.com>
2010-02-01 00:44:46 +0100

    dynamic resizing, new icons and formating

--- public/application.js
+++ public/application.js
@@ -32,6 +32,30 @@ $(function() {
 
   update_slide_height();
 
+  // Reset Font Size
+  var originalFontSize = $('html').css('font-size');
+    $(".resetFont").click(function(){
+    $('html').css('font-size', originalFontSize);
+  });
+    
+  // Increase Font Size
+  $(".increaseFont").click(function(){
+    var currentFontSize = $('html').css('font-size');
+    var currentFontSizeNum = parseFloat(currentFontSize, 10);
+    var newFontSize = currentFontSizeNum*1.2;
+    $('html').css('font-size', newFontSize);
+    return false;
+  });
+
+  // Decrease
+  $(".decreaseFont").click(function(){
+    var currentFontSize = $('html').css('font-size');
+    var currentFontSizeNum = parseFloat(currentFontSize, 10);
+    var newFontSize = currentFontSizeNum*0.8;
+    $('html').css('font-size', newFontSize);
+    return false;
+  });
+
 });
 
 function create_slides()  {
