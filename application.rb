792d34aed2d95787a093b06202a7db4f6ba24bf6
ippa <iplord@iplord.com>
2010-02-01 00:06:25 +0100

    first steps to make flaunt mountable, subclass application etc...

--- application.rb
+++ application.rb
@@ -1,46 +1,53 @@
 #!/usr/bin/ruby
-require 'rubygems'
-require 'sinatra'
+#require 'rubygems'
+#require 'sinatra'
+#require 'sinatra/base'
 
-get '/' do
-  @dirs = Dir["presentations/*"].select { |file| File.directory?(file) }.map { |dir| File.basename(dir) }
-  erb :"index"
-end
+class Flaunt < Sinatra::Base
+  @root_dir = ::File.dirname(__FILE__)
+  set :root,        @root_dir
+  set :views,       ::File.join(@root_dir, 'presentations')
 
-get '/:presentation' do
-  @title = "#{params[:presentation]} - [ Made with http://github.com/ippa/flaunt ]"
-
-  template = Dir["presentations/#{params[:presentation]}/index.*"].first
-  content = Tilt.new(template).render
-
-  #
-  # The core of Flaunt:
-  #
-  # 1) Take generated HTML, split at <hr>
-  # 2) Wrap each block with <div class="slide">block here</slide> which can be styled with jquery/css
-  #
-  final_content = ""
-  content.split(/\<hr.+\>/).each do |part|
-    next if part =~ /\A\s+\Z/ # Skip part if it's empty (only containing space)
-
-    final_content += "<div class=\"slide\">\n"
-    final_content += part
-    final_content += "\n</div>"
+  get '/' do
+    @dirs = Dir["presentations/*"].select { |file| File.directory?(file) }.map { |dir| File.basename(dir) }
+    erb :"index"
   end
 
-  erb final_content
+  get '/:presentation' do
+    views = File.join(File.dirname(__FILE__), 'presentations')  
+    @title = "#{params[:presentation]} - [ Made with http://github.com/ippa/flaunt ]"
+
+    template = Dir["#{views}/#{params[:presentation]}/index.*"].first
+    content = Tilt.new(template).render
+
+    #
+    # The core of Flaunt:
+    #
+    # 1) Take generated HTML, split at <hr>
+    # 2) Wrap each block with <div class="slide">block here</slide> which can be styled with jquery/css
+    #
+    final_content = ""
+    content.split(/\<hr.+\>/).each do |part|
+      next if part =~ /\A\s+\Z/ # Skip part if it's empty (only containing space)
+  
+      final_content += "<div class=\"slide\">\n"
+      final_content += part
+      final_content += "\n</div>"
+    end
+  
+    erb final_content
 
 
-  #
-  # Sinatra < 1.0 - Before tilt
-  #
-  # content = open(template).read
-  # html = case File.extname(index)
-  #   when ".erb"       then require 'erb';       ERB.new(content, 0, "%<>").result
-  #   when ".haml"      then require 'haml';      Haml::Engine.new(content).render
-  #   when ".textile"   then require 'RedCloth';  RedCloth.new(content).to_html
-  #   when ".md"        then require 'maruku';    Maruku.new(content).to_html
-  # end
-  # erb html  # render the produced HTML with our layout.erb
+    #
+    # Sinatra < 1.0 - Before tilt
+    #
+    # content = open(template).read
+    # html = case File.extname(index)
+    #   when ".erb"       then require 'erb';       ERB.new(content, 0, "%<>").result
+    #   when ".haml"      then require 'haml';      Haml::Engine.new(content).render
+    #   when ".textile"   then require 'RedCloth';  RedCloth.new(content).to_html
+    #   when ".md"        then require 'maruku';    Maruku.new(content).to_html
+    # end
+    # erb html  # render the produced HTML with our layout.erb
+  end
 end
-
--- config.ru
+++ config.ru
@@ -2,12 +2,5 @@
 require 'rubygems'
 require 'sinatra'
 require 'application'
-
-root_dir = ::File.dirname(__FILE__)
-set :root,        root_dir
-set :app_file,    ::File.join(root_dir, 'application.rb')
-set :views,       ::File.join(root_dir, 'presentations')
-disable :run
-
-run Sinatra::Application
+run Flaunt
 
